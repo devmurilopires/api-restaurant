4eabfd4626bec5a55f442bfeaa91f1fe163662ff
Murilo Pires <murilopiresoficial@gmail.com>
2026-07-20 12:08:07 -0300

    Adicionando o remove de products

--- src/controllers/products-controller.ts
+++ src/controllers/products-controller.ts
@@ -1,6 +1,7 @@
 import { NextFunction, Request, Response } from "express";
-import { number, z } from "zod";
+import { z } from "zod";
 import { knex } from "@/database/knex"
+import { AppError } from "@/utils/AppError";
 
 class ProductController {
     async index(req: Request, res: Response, next: NextFunction) {
@@ -50,10 +51,46 @@ class ProductController {
             })
 
             const {name, price} = bodySchema.parse(req.body)
+
+            const product = await knex<ProductRepository>("products")
+            .select()
+            .where({ id })
+            .first()
+
+            if (!product) {
+                throw new AppError("Product not found")
+            }
+
             await knex<ProductRepository>("products").update({ name, price, updated_at: knex.fn.now()}).where({ id })
 
             return res.json()
             
+        } catch (error) {
+            next(error)
+        }
+    }
+
+    async remove(req: Request, res: Response, next: NextFunction){
+        try {
+            const id = z
+            .string()
+            .transform((value) => Number(value))
+            .refine((value) => !isNaN(value), { message: "id must be a number"})
+            .parse(req.params.id)
+
+            const product = await knex<ProductRepository>("products")
+            .select()
+            .where({ id })
+            .first()
+
+            if (!product) {
+                throw new AppError("Product not found")
+            }
+            
+            await knex<ProductRepository>("products").delete().where({ id })
+
+            return res.json()
+
         } catch (error) {
             next(error)
         }
--- src/utils/AppError.ts
+++ src/utils/AppError.ts
@@ -2,7 +2,7 @@ class AppError {
     menssage: string
     statusCode: number
 
-    constructor (message: string, statusCode: number) {
+    constructor (message: string, statusCode: number = 400) {
         this.menssage = message
         this.statusCode = statusCode
     }
